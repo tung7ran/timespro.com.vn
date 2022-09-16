20ea8a2dcaf12c1151b5f9dde29357485a09af25
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-09-16 17:15:26 +0700

    update__02

--- js/main.js
+++ js/main.js
@@ -3,9 +3,47 @@ $(document).ready(function(){
 
     $('.btn--more').click(function(){
         $('.open--options').toggle();
-    })
+    });
+    /**
+     * tab UI -> Home
+     */
+ 
+    $('.bds--options--heading__list bds--options--heading__list--item:first-child').addClass('active');
+    $('.bds--options--container').hide();
+    $('.bds--options--container:first').show();
 
-    // slick slide
+    // Click function
+    $('.bds--options--heading__list--item').click(function(){
+    $('.bds--options--heading__list--item').removeClass('active');
+    $(this).addClass('active');
+    $('.bds--options--container').hide();
+
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+    });
+
+    /**
+     * Tab UI page duan
+     */
+    $('.project--heading--list__item:first-child').addClass('active');
+    $('.project--content').hide();
+    $('.project--content:first').show();
+    
+    // Click function
+    $('.project--heading--list__item').click(function(){
+    $('.project--heading--list__item').removeClass('active');
+    $(this).addClass('active');
+    $('.project--content').hide();
+    
+    var activeTabProject = $(this).find('a').attr('href');
+    $(activeTabProject).fadeIn();
+    return false;
+    });
+    /**
+     * slick slide -> HOME
+     */
+ 
     $('.investor--group').slick({
             infinite: true,
             slidesToShow: 5,
@@ -16,4 +54,15 @@ $(document).ready(function(){
             nextArrow: "<button type='button' class='slick-next pull-left'><i class='fa-sharp fa-solid fa-angles-right'></i></button>",
 
     });
+
+      $(".select-control-label>a").click (function(){
+        // Close all open windows
+        $(".custom-dropbox-cate").stop().slideUp(300); 
+        // Toggle this window open/close
+        $(this).next(".custom-dropbox-cate").stop().slideToggle(300);
+        //hitter test// 
+        // $(".hitter").show()
+      });
+      
+     
 });
